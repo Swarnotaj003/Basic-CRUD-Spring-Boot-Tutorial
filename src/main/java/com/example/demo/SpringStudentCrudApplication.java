1b15c975ec2520e739e4878d9688d87567ad60f6
Swarnotaj003 <swarnotajk@gmail.com>
2024-12-21 23:00:40 +0530

    Undo changes

--- src/main/java/com/example/demo/SpringStudentCrudApplication.java
+++ src/main/java/com/example/demo/SpringStudentCrudApplication.java
@@ -3,22 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class SpringStudentCrudApplication {
 
-	public static void main(String[] args) {
-		// Load .env file
-        Dotenv dotenv = Dotenv.load();
-        
-        // Set environment variables as system properties
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        
+	public static void main(String[] args) {        
 		SpringApplication.run(SpringStudentCrudApplication.class, args);
 	}
 
