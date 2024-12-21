e7c43672e8298d6d6b7dccbd4a770a13fa038757
Swarnotaj003 <swarnotajk@gmail.com>
2024-12-21 23:12:56 +0530

    Shifted DB to cloud

--- src/main/java/com/example/demo/SpringStudentCrudApplication.java
+++ src/main/java/com/example/demo/SpringStudentCrudApplication.java
@@ -3,10 +3,22 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SpringStudentCrudApplication {
 
-	public static void main(String[] args) {        
+	public static void main(String[] args) {		
+		// Load the .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Access the variables
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(SpringStudentCrudApplication.class, args);
 	}
 
